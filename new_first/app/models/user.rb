612b1f09e7cf78ad0206dc8b48c72eddba9a41b5
ashap94 <aaronshapiro94@gmail.com>
2019-08-01 12:12:32 -0700

    Add seed files

--- new_first/app/models/user.rb
+++ new_first/app/models/user.rb
@@ -2,4 +2,15 @@ class User < ApplicationRecord
   
   validates :username, presence: true, uniqueness: true
 
+  has_many :artworks,
+    foreign_key: :artist_id,
+    class_name: :Artwork
+
+  has_many :artwork_shares,  #returns list of artwork that this use shared
+    foreign_key: :viewer_id,
+    class_name: :ArtworkShare
+
+  has_many :shared_artworks, #returns list of artwork shared with this user
+    through: :artwork_shares,
+    source: :artwork
 end
--- new_first/db/seeds.rb
+++ new_first/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+
+  ArtworkShare.destroy_all
+  Artwork.destroy_all
+  User.destroy_all
+
+  fue = User.create!(username: 'fue')
+  aaron = User.create!(username: 'ayron')
+  mike = User.create!(username: 'coolboi420')
+
+  art1 = Artwork.create!(title: 'Mona Lisa', image_url: 'www.fue_mona_lisa.com', artist_id: fue.id )
+  art2 = Artwork.create!(title: 'Mona Lisa', image_url: 'www.aaron_mona_lisa.com', artist_id: aaron.id )
+  art3 = Artwork.create!(title: 'Superman', image_url: 'www.Superman.com', artist_id: fue.id )
+  art4 = Artwork.create!(title: 'Dr. Manhatten', image_url: 'www.BlueMan.com', artist_id: aaron.id )
+  art5 = Artwork.create!(title: 'Mars', image_url: 'www.Mars.com', artist_id: mike.id )
+
+  share1 = ArtworkShare.create!(artwork_id: art1.id, viewer_id: aaron.id )
+  share2 = ArtworkShare.create!(artwork_id: art2.id, viewer_id: fue.id )
+  share3 = ArtworkShare.create!(artwork_id: art3.id, viewer_id: mike.id )
+  share4 = ArtworkShare.create!(artwork_id: art3.id, viewer_id: aaron.id )
+  share5 = ArtworkShare.create!(artwork_id: art4.id, viewer_id: fue.id )
+  share6 = ArtworkShare.create!(artwork_id: art4.id, viewer_id: aaron.id )
+  share7 = ArtworkShare.create!(artwork_id: art5.id, viewer_id: fue.id )
+
+end
